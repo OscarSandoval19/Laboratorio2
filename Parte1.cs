468fbaf5c476c4a3e1bc14f8b9e9f7e6b3b927ec
Oscar Guillermo Sandoval García <osandovalg1@miumg.edu.gt>
2025-02-14 15:25:01 -0600

    Add files via upload

---
+++ Parte1.cs
@@ -0,0 +1,24 @@
+﻿int numeroEntero;
+double numeroDecimal;
+string texto;
+bool valorBooleano;
+
+
+Console.Write("Ingrese un número entero: ");
+numeroEntero = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese un número decimal: ");
+numeroDecimal = double.Parse(Console.ReadLine());
+
+Console.Write("Ingrese un texto: ");
+texto = Console.ReadLine();
+
+Console.Write("Ingrese un valor verdadero o falso: ");
+valorBooleano = bool.Parse(Console.ReadLine());
+
+
+Console.WriteLine("\nValores ingresados:");
+Console.WriteLine($"Número entero: {numeroEntero}");
+Console.WriteLine($"Número decimal: {numeroDecimal}");
+Console.WriteLine($"Texto: {texto}");
+Console.WriteLine($"Valor booleano: {valorBooleano}");
