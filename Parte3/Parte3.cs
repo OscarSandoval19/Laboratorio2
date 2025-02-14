ff420a3b7dbe118e88e2629d6317becb8d8d72be
USUARIOTC <USUARIOTC@DESKTOP-FEN742K>
2025-02-14 14:52:06 -0600

    Agregar archivos de proyecto.

---
+++ Parte3/Parte3.cs
@@ -0,0 +1,16 @@
+﻿int num1, num2;
+
+
+Console.Write("Ingrese el primer número: ");
+num1 = int.Parse(Console.ReadLine());
+Console.Write("Ingrese el segundo número: ");
+num2 = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"{num1} > {num2} = {num1 > num2}");
+Console.WriteLine($"{num1} < {num2} = {num1 < num2}");
+Console.WriteLine($"{num1} == {num2} = {num1 == num2}");
+
+
+Console.WriteLine($"({num1} > 0 && {num2} > 0) = {(num1 > 0 && num2 > 0)}");
+Console.WriteLine($"({num1} > 0 || {num2} > 0) = {(num1 > 0 || num2 > 0)}");
